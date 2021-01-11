31652d770192f3894a944452da6e58faa0e5bd3f
Sarah-56 <59021799+Sarah-56@users.noreply.github.com>
2021-01-11 02:11:45 +0200

    add flatbutton to end.dart

--- flutter_app/lib/Pages/end.dart
+++ flutter_app/lib/Pages/end.dart
@@ -4,32 +4,33 @@ class done extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        elevation: 0.1,
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(''),
-      ),
       body: Center(
-        child: Column(
-          children: [
-            new Image.asset('images/done-icon-10.jpg', //================================photo====================================================
-              width: 300.0,
-              height: 300.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(100.0),
-              child: Text(
-                "The payment is done.",
-                style: TextStyle(color: Colors.grey[500], fontWeight: FontWeight.bold),
+          child: Column(
+            children: [
+              SizedBox(height: 60.0,),
+              new Image.asset('images/done-icon-10.jpg', //================================photo====================================================
+                width: 300.0,
+                height: 300.0,
               ),
-            ),
+              Padding(
+                padding: const EdgeInsets.all(80.0),
+                child: Text(
+                  "The payment is done.",
+                  style: TextStyle(color: Colors.grey[500], fontWeight: FontWeight.bold),
+                ),
+              ),
+              FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                int count = 0;
+                Navigator.of(context).popUntil((_) => count ++ >= 5);
+               }, child: Text('Back to home')),
+            ],
+          ),
 
-          ],
         ),
-
-      ),
-      backgroundColor: Colors.white,
     );
+
+
   }
 }
